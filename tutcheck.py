e4d5a59d2efccfbfc2c3e55926e5465037347012
Christoph B. Rist <rist.christoph@gmail.com>
2015-11-14 13:58:09 +0100

    Switch to python 3, standardize indentation, use PEP style

--- tutcheck.py
+++ tutcheck.py
@@ -16,73 +16,74 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import urllib
-from AppKit import NSSound
-from time import sleep
-
-def alert():
-	class sound:
-	    def __init__(self, file):
-	        self._sound = NSSound.alloc()
-	        self._sound.initWithContentsOfFile_byReference_(file, True)
-	    def play(self): self._sound.play()
-	    def stop(self): self._sound.stop()
-	    def is_playing(self): return self._sound.isPlaying()
-
-	for i in range(100):
-	        s = sound("hurricane_ton3.wav")
-	        s.play()
-	        sleep(3)
-
-def time(sec):
-	if float(sec) < 10:
-		sec1 = "0"+str(sec)
-	else:
-		sec1 = str(sec)
-
-	return sec1
-
+import urllib.request
 from time import localtime, sleep
 
-print "*** TutCheck by Sebastian Müller ***"
-useold = raw_input("Use last website for comparison? (Y/N) ")
+
+def alert():
+    class Sound:
+        def __init__(self, file):
+            self._sound = NSSound.alloc()
+            self._sound.initWithContentsOfFile_byReference_(file, True)
+
+        def play(self): self._sound.play()
+
+        def stop(self): self._sound.stop()
+
+        def is_playing(self): return self._sound.isPlaying()
+
+    for i in range(100):
+        s = Sound("hurricane_ton3.wav")
+        s.play()
+        sleep(3)
+
+
+def time(sec):
+    if float(sec) < 10:
+        sec1 = "0" + str(sec)
+    else:
+        sec1 = str(sec)
+
+    return sec1
+
+
+print("*** TutCheck by Sebastian Müller ***")
+useold = input("Use last website for comparison? (Y/N) ")
 if useold == "Y":
-	d = open("orig.html", "r")
-	orig = d.read()
-	d.close()
-	d = open("url.txt", "r")
-	url = d.read()
-	d.close()
+    d = open("orig.html", "r")
+    orig = d.read()
+    d.close()
+    d = open("url.txt", "r")
+    url = d.read()
+    d.close()
 else:
-	url = raw_input("Target URL: ")
-	print "URL:", url
-	f = urllib.urlopen(url)
-	orig = f.read()
-	f.close()
-	d = open("orig.html", "w")
-	d.write(orig)
-	d.close
-  	d = open("url.txt", "w")
-	d.write(url)
-	d.close
+    url = input("Target URL: ")
+    print("URL:", url)
+    f = urllib.request.urlopen(url)
+    orig = f.read()
+    f.close()
+    d = open("orig.html", "w")
+    d.write(orig)
+    d.close()
+    d = open("url.txt", "w")
+    d.write(url)
+    d.close()
 
-
-print "Original website has been cached for comparison."
+print("Original website has been cached for comparison.")
 
 while True:
-	time = localtime()
-	sec = time(time[5])
-	hour = time(time[3])
-	minute = time(time[4])
-	loct = hour+":"+minute+":"+sec
+    time = localtime()
+    sec = time(time[5])
+    hour = time(time[3])
+    minute = time(time[4])
+    loct = hour + ":" + minute + ":" + sec
 
-	g = urllib.urlopen(url)
-	comp = g.read()
-	g.close()
+    g = urllib.request.urlopen(url)
+    comp = g.read()
+    g.close()
 
-	if comp != orig:
-		print loct, "!ALERT! Website has changed !ALERT!"
-		alert()
-	else:
-		print loct, "No changes yet"
-
+    if comp != orig:
+        print(loct, "!ALERT! Website has changed !ALERT!")
+        alert()
+    else:
+        print(loct, "No changes yet")
